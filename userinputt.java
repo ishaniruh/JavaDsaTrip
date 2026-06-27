14e9bc4aa9703460fefb270cf0fe154106d3c6af
ishaniruh <ishaniruhela1@gmail.com>
2026-06-27 19:02:44 +0530

    add user input demonstration in Java

---
+++ userinputt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class userinputt {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = sc.nextLine();
+        System.out.println("enter your age");
+        int age = sc.nextInt();
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
