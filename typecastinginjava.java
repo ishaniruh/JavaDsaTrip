67901ad649cccedc199da2fdb0c2e8dfe4dbe839
ishaniruh <ishaniruhela1@gmail.com>
2026-06-27 18:38:39 +0530

    add typecasting example in Java

---
+++ typecastinginjava.java
@@ -0,0 +1,18 @@
+public class typecastinginjava {
+    public static void main(String args[]){
+        int a = 10;
+        float c = a;
+        double d = a;
+        double b = 23.999;
+        int e = (int)b;
+        System.out.println("implicit typecasting ");
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("explicit typecasting");
+        System.out.println(e);
+
+
+    }
+    
+}
