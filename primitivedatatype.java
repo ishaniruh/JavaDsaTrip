6702eb8e81638bbd474df556ad5ada53dfac22fd
ishaniruh <ishaniruhela1@gmail.com>
2026-06-27 18:55:44 +0530

    add primitive data type demonstration in Java

---
+++ primitivedatatype.java
@@ -0,0 +1,14 @@
+class primitivedatatype{
+    public static void main(String args[]){
+        int a = 20;
+        char b = 'a';
+        float  c= 10.2f;
+        double d = 30003.3;
+        System.out.println("int" + " " +a);
+        System.out.println("char" + " " +b);
+        System.out.println("float" +" " + c);
+        System.out.println("double" +" " + d);
+
+
+    }
+}
