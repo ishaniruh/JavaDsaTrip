8462f33e3ff20c569618fa0fe22b4038c28f4798
ishaniruh <ishaniruhela1@gmail.com>
2026-06-27 19:10:05 +0530

    add wrapper class example in Java

---
+++ wrapperclass.java
@@ -0,0 +1,10 @@
+// wrapper class is used to convert primitive data type into object and vice versa.
+public class wrapperclass {
+    public static void main(String args[]){
+        Integer a = 10;
+        Character b = 'a';
+        System.out.println(a.intValue());
+        System.out.println(b.charValue());
+    }
+    
+}
