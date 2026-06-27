02c7535f1ad9c8ef3c64e7fe1104200e6bb94f36
ishaniruh <ishaniruhela1@gmail.com>
2026-06-27 18:26:30 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String args[]){
+        System.out.println("Hello,World");
+        System.out.println("i am starting my journey in java and dsa");
+    }
+    
+}
